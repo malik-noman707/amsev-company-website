7ffee72d4216f717290aa192fa6894fed20508b6
malik-noman707 <malik.noman@amsev.com>
2024-12-02 21:47:54 +0500

    check

---
+++ build/precache-manifest.6f1d3498de4709d973d0da37f2bebc01.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31264195f6bbe54da38088cd3c6d6c71",
+    "url": "/index.html"
+  },
+  {
+    "revision": "40633002389748ba8ead",
+    "url": "/static/css/2.b24350a8.chunk.css"
+  },
+  {
+    "revision": "5dc9331b92590f72a27d",
+    "url": "/static/css/main.431b0579.chunk.css"
+  },
+  {
+    "revision": "40633002389748ba8ead",
+    "url": "/static/js/2.541052e6.chunk.js"
+  },
+  {
+    "revision": "5dc9331b92590f72a27d",
+    "url": "/static/js/main.436bcb3c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
