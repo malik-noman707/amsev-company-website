a112a7fe17a15b4a585332dffbd5b0bc812d0f37
malik-noman707 <malik.noman@amsev.com>
2024-12-02 21:56:12 +0500

    contact us updated

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -11,7 +11,7 @@ const initialState = {
 };
 
 export const Contact = (props) => {
-  const [{ name, email, message }, setState] = useState(initialState);
+  const [ setState] = useState(initialState);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
