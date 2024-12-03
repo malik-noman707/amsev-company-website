91d96d5b7459a3567a618a928ebec42696852006
malik-noman707 <malik.noman@amsev.com>
2024-12-03 18:01:25 +0500

    nav update

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -17,7 +17,7 @@ export const Navigation = (props) => {
             <span className="icon-bar"></span>{" "}
             <span className="icon-bar"></span>{" "}
           </button>
-          <a  className="navbar-brand page-scroll" href="#page-top" style={{ color: '#3D77B1', fontSize: '40px', letterSpacing: '3px' }}>
+          <a  className="navbar-brand page-scroll" href="https://amsev.com/" style={{ color: '#3D77B1', fontSize: '40px', letterSpacing: '3px' }}>
             AMSEV
           </a>
         </div>
